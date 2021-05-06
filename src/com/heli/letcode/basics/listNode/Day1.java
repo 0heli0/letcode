682e8a1f0697d770fcc8a40650ab1e4bed5ea02b
heli <heli@scnx.com>
2021-05-06 16:24:11 +0800

    a

---
+++ src/com/heli/letcode/basics/listNode/Day1.java
@@ -0,0 +1,213 @@
+package com.heli.letcode.basics.listNode;
+
+import java.util.Stack;
+
+/**
+ * @Author: YourName
+ * @Date: 2021/5/6
+ */
+public class Day1 {
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode pre = head;
+        int last = length(head) - n;
+        //如果last等于0表示删除的是头结点
+        if (last == 0)
+            return head.next;
+        //这里首先要找到要删除链表的前一个结点
+        for (int i = 0; i < last - 1; i++) {
+            pre = pre.next;
+        }
+        //然后让前一个结点的next指向要删除节点的next
+        pre.next = pre.next.next;
+        return head;
+    }
+
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public ListNode reverseList(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        //把链表节点全部摘掉放到栈中
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+        if (stack.isEmpty())
+            return null;
+        ListNode node = stack.pop();
+        ListNode dummy = node;
+        //栈中的结点全部出栈，然后重新连成一个新的链表
+        while (!stack.isEmpty()) {
+            ListNode tempNode = stack.pop();
+            node.next = tempNode;
+            node = node.next;
+        }
+        //最后一个结点就是反转前的头结点，一定要让他的next
+        //等于空，否则会构成环
+        node.next = null;
+        return dummy;
+    }
+
+    //求链表的长度
+    private int length(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //下面4行是空判断
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            //比较一下，哪个小就把哪个放到新的链表中
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        //然后把那个不为空的链表挂到新的链表中
+        curr.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        //通过快慢指针找到中点
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        //如果fast不为空，说明链表的长度是奇数个
+        if (fast != null) {
+            slow = slow.next;
+        }
+        //反转后半部分链表
+        slow = reverse(slow);
+
+        fast = head;
+        while (slow != null) {
+            //然后比较，判断节点值是否相等
+            if (fast.val != slow.val)
+                return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    //反转链表
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) {
+     * val = x;
+     * next = null;
+     * }
+     * }
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        //快慢两个指针
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            //慢指针每次走一步
+            slow = slow.next;
+            //快指针每次走两步
+            fast = fast.next.next;
+            //如果相遇，说明有环，直接返回true
+            if (slow == fast)
+                return true;
+        }
+        //否则就是没环
+        return false;
+
+    }
+
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(4);
+        node.next.next = new ListNode(5);
+        node.next.next.next = new ListNode(9);
+
+        deleteNode(node);
+
+        System.out.println(node.val);
+        System.out.println(node.next.val);
+        System.out.println(node.next.next.val);
+
+
+    }
+}
---
+++ src/com/heli/letcode/basics/listNode/ListNode.java
@@ -0,0 +1,14 @@
+package com.heli.letcode.basics.listNode;
+
+/**
+ * @Author: YourName
+ * @Date: 2021/5/6
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
