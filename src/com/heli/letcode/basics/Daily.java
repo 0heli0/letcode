31ba5bb943c3f8aad08b1a2d55ffa789d48afd52
heli <heli@scnx.com>
2021-05-18 09:14:03 +0800

    无重复字符的最长子串

--- src/com/heli/letcode/basics/Daily.java
+++ src/com/heli/letcode/basics/Daily.java
@@ -37,9 +37,31 @@ public class Daily {
         return result;
     }
 
+    /**
+     * 无重复字符的最长子串
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int maxNum = 0;
+        if (s == null || s.length() == 0) return maxNum;
+        if (s.length() == 1) return 1;
+        char[] bytes = s.toCharArray();
+        int index = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < bytes.length; i++) {
+            if(map.containsKey(bytes[i])){
+                index = Math.max(index, map.get(bytes[i]) +1);
+            }
+            map.put(bytes[i],i);
+            maxNum = Math.max(maxNum, i - index +1);
+        }
+        return maxNum;
+    }
+
     public static void main(String[] args) {
         Daily daily = new Daily();
-        int result = daily.romanToInt("III");
+        int result = daily.lengthOfLongestSubstring("abcabcdabca");
         System.out.println(result);
     }
 
